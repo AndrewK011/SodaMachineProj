7e809aa06e56a6304eb12ea78622f8285e39e5b3
Andrew K <drummer4life93@yahoo.com>
2020-05-19 11:42:24 -0500

    Added MakeChange methods and related methods that return list of coins based on how much change is left.

--- SodaMachine/SodaMachine.cs
+++ SodaMachine/SodaMachine.cs
@@ -60,5 +60,141 @@ namespace SodaMachine
                 inventory.Add(orangeSoda);
             }
         }
+
+        public List<Coin> GiveChangeBack(List<Coin> coins)
+        {
+            return coins;
+        }
+
+        public List<Coin> MakeChange(List<Coin> coinsInRegister, double coinsMinusCost)
+        {
+            List<Coin> coinsToGiveBack = new List<Coin>();
+
+            while (coinsMinusCost != 0)
+            {
+                if (coinsMinusCost / 0.25 != 0)
+                {
+                    coinsMinusCost = MakeQuarterChange(coinsToGiveBack, coinsMinusCost, coinsInRegister);
+                }
+
+                else if (coinsMinusCost / 0.1 != 0)
+                {
+                    coinsMinusCost = MakeDimeChange(coinsToGiveBack, coinsMinusCost, coinsInRegister);
+                }
+
+                else if (coinsMinusCost / 0.05 != 0)
+                {
+                    coinsMinusCost = MakeNickelChange(coinsToGiveBack, coinsMinusCost, coinsInRegister);
+                }
+
+                else
+                {
+                    coinsMinusCost = MakePennyChange(coinsToGiveBack, coinsMinusCost, coinsInRegister);
+                }
+
+            }
+            
+
+
+            return coinsToGiveBack;
+        }
+
+        public double MakeQuarterChange(List<Coin> coinsToGiveBack, double coinsMinusCost, List<Coin> coinsInRegister)
+        {
+            int divisibleByQuarter = (int)(coinsMinusCost / 0.25);
+            double changeRemaining = coinsMinusCost % 0.25;
+            int counter = 0;
+
+            for (int i = 0; i < coinsInRegister.Count; i++)
+            {
+                if (coinsInRegister[i].name == "quarter")
+                {
+                    coinsToGiveBack.Add(coinsInRegister[i]);
+                    coinsInRegister.RemoveAt(i);
+                    counter++;
+                }
+
+                if (counter == divisibleByQuarter)
+                {
+                    break;
+                }
+
+            }
+            return changeRemaining;
+        }
+
+        public double MakeDimeChange(List<Coin> coinsToGiveBack, double coinsMinusCost, List<Coin> coinsInRegister)
+        {
+            int divisibleByDime = (int)(coinsMinusCost / 0.1);
+            double changeRemaining = coinsMinusCost % 0.1;
+            int counter = 0;
+
+            for (int i = 0; i < coinsInRegister.Count; i++)
+            {
+                if (coinsInRegister[i].name == "dime")
+                {
+                    coinsToGiveBack.Add(coinsInRegister[i]);
+                    coinsInRegister.RemoveAt(i);
+                    counter++;
+                }
+
+                if (counter == divisibleByDime)
+                {
+                    break;
+                }
+
+            }
+            return changeRemaining;
+        }
+
+        public double MakeNickelChange(List<Coin> coinsToGiveBack, double coinsMinusCost, List<Coin> coinsInRegister)
+        {
+            int divisibleByNickel = (int)(coinsMinusCost / 0.05);
+            double changeRemaining = coinsMinusCost % 0.05;
+            int counter = 0;
+
+            for (int i = 0; i < coinsInRegister.Count; i++)
+            {
+                if (coinsInRegister[i].name == "nickel")
+                {
+                    coinsToGiveBack.Add(coinsInRegister[i]);
+                    coinsInRegister.RemoveAt(i);
+                    counter++;
+                }
+
+                if (counter == divisibleByNickel)
+                {
+                    break;
+                }
+
+            }
+            return changeRemaining;
+        }
+
+        public double MakePennyChange(List<Coin> coinsToGiveBack, double coinsMinusCost, List<Coin> coinsInRegister)
+        {
+            int divisibleByPenny = (int)(coinsMinusCost / 0.01);
+            double changeRemaining = coinsMinusCost % 0.01;
+            int counter = 0;
+
+            for (int i = 0; i < coinsInRegister.Count; i++)
+            {
+                if (coinsInRegister[i].name == "penny")
+                {
+                    coinsToGiveBack.Add(coinsInRegister[i]);
+                    coinsInRegister.RemoveAt(i);
+                    counter++;
+                }
+
+                if (counter == divisibleByPenny)
+                {
+                    break;
+                }
+
+            }
+            return changeRemaining;
+        }
+
+
     }
 }
